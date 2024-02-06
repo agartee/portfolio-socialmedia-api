97f836acd2bae7ed8fd3dcb17796e86426acec06
Adam Gartee <agartee@gmail.com>
2024-02-06 05:50:22 -0500

    azure devops pipeline updates

---
+++ src/SocialMedia.Domain/Commands/CreatePostHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SocialMedia.Domain.Models;
+using SocialMedia.Domain.Services;
+
+namespace SocialMedia.Domain.Commands
+{
+    public class CreatePostHandler : IRequestHandler<CreatePost, PostInfo>
+    {
+        private readonly IPostRepository postRepository;
+        private readonly IUserContext userContext;
+
+        public CreatePostHandler(IPostRepository postRepository, IUserContext userContext)
+        {
+            this.postRepository = postRepository;
+            this.userContext = userContext;
+        }
+
+        public async Task<PostInfo> Handle(CreatePost request, CancellationToken cancellationToken)
+        {
+            var post = new Post
+            {
+                Id = PostId.NewId(),
+                AuthorUserId = userContext.UserId,
+                Text = request.Text,
+                Created = DateTime.UtcNow
+            };
+
+            return await postRepository.CreatePost(post, cancellationToken);
+        }
+    }
+}
